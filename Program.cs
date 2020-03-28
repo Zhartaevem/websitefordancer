38b78b1239a4b76f76e7075c3aac6d084767ad2d
zhartaevem <zhartaevem@gmail.com>
2020-03-28 21:29:18 +0500

    1

--- Program.cs
+++ Program.cs
@@ -12,18 +12,34 @@ namespace WebSiteForDancer
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    CreateWebHostBuilder(args).Build().Run();
+        //}
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        //{
+        //    var port = Environment.GetEnvironmentVariable("PORT");
+
+        //    return WebHost.CreateDefaultBuilder(args)
+        //                  .UseStartup<Startup>()
+        //                  .UseUrls("http://*:" + port);
+        //}
+
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
         {
             var port = Environment.GetEnvironmentVariable("PORT");
 
-            return WebHost.CreateDefaultBuilder(args)
-                          .UseStartup<Startup>()
-                          .UseUrls("http://*:" + port);
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>().UseUrls("http://*:" + port);
+            });
         }
 
     }
