35543d20b8af1168b771cdefdcb8bc671eee2176
zhartaevem <zhartaevem@gmail.com>
2020-03-28 21:57:52 +0500

    again

--- Dockerfile
+++ Dockerfile
@@ -33,8 +33,7 @@ RUN dotnet publish -c Release -o out
 FROM mcr.microsoft.com/dotnet/core/runtime:3.1
 WORKDIR /app
 COPY --from=build-env /app/out .
-#CMD dotnet WebSiteForDancer.dll
-ENTRYPOINT ["dotnet", "WebSiteForDancer.dll"]
+CMD dotnet WebSiteForDancer.dll
 
 
 
--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace WebSiteForDancer
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
