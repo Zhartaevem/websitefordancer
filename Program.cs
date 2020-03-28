a046a841fc07d6eb1fb2e94374a79e5963ca6d2a
zhartaevem <zhartaevem@gmail.com>
2020-03-28 17:49:53 +0500

    Configure

--- Program.cs
+++ Program.cs
@@ -17,8 +17,7 @@ namespace WebSiteForDancer
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
