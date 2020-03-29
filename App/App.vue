dd3f833df6f11ea6448bab47d086229e5e371cf8
zhartaevem <zhartaevem@gmail.com>
2020-03-29 16:56:58 +0500

    Add vue js

---
+++ App/App.vue
@@ -0,0 +1,30 @@
+﻿<template>
+    <div class="container">
+        <div class="header">
+            <!--<header class="wrap header_wrap">
+                <div class="header_top">
+                    <div class="header_logo">
+                        <img src="images/solar_system.png" />
+                        <p class="evaa_font">FLIGHT</p>
+                    </div>
+                    <div class="header_buttons"></div>
+                </div>
+                <div class="header_middle">
+                    <h1>полет на марс</h1>
+                </div>
+                <div class="header_bottom">
+                    <div class="header_buy_button"></div>
+                </div>
+            </header>-->
+        </div>
+        <div>
+            <img src="images/Gotovy_Sayt3.png" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default { }
+</script>
+
+<style lang="scss"></style>
