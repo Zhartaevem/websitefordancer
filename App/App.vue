8916a56ee8a6050da4581a72807f6ab9913d7f33
zhartaevem <zhartaevem@gmail.com>
2020-04-04 21:39:16 +0500

    First front end work

--- App/App.vue
+++ App/App.vue
@@ -1,21 +1,6 @@
-﻿<template>
+﻿<!--<template>
     <div class="container">
         <div class="header">
-            <!--<header class="wrap header_wrap">
-                <div class="header_top">
-                    <div class="header_logo">
-                        <img src="images/solar_system.png" />
-                        <p class="evaa_font">FLIGHT</p>
-                    </div>
-                    <div class="header_buttons"></div>
-                </div>
-                <div class="header_middle">
-                    <h1>полет на марс</h1>
-                </div>
-                <div class="header_bottom">
-                    <div class="header_buy_button"></div>
-                </div>
-            </header>-->
         </div>
         <div>
             <img src="images/Gotovy_Sayt3.png" />
@@ -24,7 +9,81 @@
 </template>
 
 <script>
-    export default { }
+    export default {}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss"></style>-->
+
+
+<template>
+    <div class="container">
+        <div class="header">
+            <header class="wrap header_wrap">
+                <div class="header_border_wrap_big">
+                    <div class="header_big_container">
+                        <div class="header_border_wrap_small">
+                            <div class="header_middle">
+                                <h1>Я — ЮЛИЯ ГОРБУНОВА</h1>
+                                <p class="robotomedium_font">Хореограф, участница шоу-балета</p>
+                                <p class="robotomedium_font">—</p>
+                                <p class="roboto_font">Танец — это то, что у тебя внутри.</p>
+                                <p class="roboto_font">Оно или есть, или его нет.</p>
+                            </div>
+                            <!--<div class="header_bottom">
+                        <div class="header_buy_button"></div>
+                    </div>-->
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="header_photo_wrap">
+                <img class="header_photo" src="images/HeaderPhoto.png" />
+            </div>
+        </div>
+        <!--<div class="header">
+            <header class="wrap header_wrap">
+                <div class="header_top">
+                    <div class="header_logo"></div>
+                    <div class="header_buttons"></div>
+                </div>
+                <div class="header_middle">
+                    <h1>ПОЛЕТ НА МАРС</h1>
+                </div>
+                <div class="header_bottom">
+                    <div class="header_buy_button"></div>
+                </div>
+            </header>
+        </div>-->
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+</style>
+<!--<template>
+    <div class="container">
+        <div class="header">
+        </div>
+        <div>
+            <img src="images/Gotovy_Sayt3.png" />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss"></style>-->
+
+
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+</style>
